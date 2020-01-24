be3856a553c44f7bfc2a1abfa58489638b3344d1
Ilya Klishin <klishin.it@gmail.com>
2020-01-25 00:19:35 +0300

    Refactoring of methods in SimpleRegExpService. Reflection was deleted, try-with-resources was added. Also the way of getting the file was changed. {createFile(String path, String name)} method in {SimpleFileRepository} failed test, other methods passed test.

--- src/main/java/com/epam/izh/rd/online/service/SimpleRegExpService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleRegExpService.java
@@ -1,9 +1,7 @@
 package com.epam.izh.rd.online.service;
 
 import java.io.*;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -19,33 +17,26 @@ public class SimpleRegExpService implements RegExpService {
     @Override
     public String maskSensitiveData() {
         StringBuilder result = new StringBuilder();
-        URL resource = this.getClass().getResource("/sensitive_data.txt");
-        if (resource != null) {
-            try {
-                FileInputStream fileInputStream = new FileInputStream(Paths.get(resource.toURI()).toFile());
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
-                String line;
-                String foundedNumber;
-                String replacedNumber;
-                Pattern pattern = Pattern.compile("(\\d{4}\\s\\d{4}\\s\\d{4}\\s\\d{4})");
-                while ((line = bufferedReader.readLine()) != null) {
-                    Matcher matcher = pattern.matcher(line);
-                    while (matcher.find()) {
-                        foundedNumber = matcher.group();
-                        replacedNumber = foundedNumber.split(" ")[0] + " **** **** " + foundedNumber.split(" ")[3];
-                        line = line.replaceAll(foundedNumber, replacedNumber);
-                    }
-                    result.append(line);
+        File file = new File("src/main/resources/sensitive_data.txt".replaceAll("/", File.separator));
+        try (FileInputStream fileInputStream = new FileInputStream(file);
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream, StandardCharsets.UTF_8))) {
+            String line;
+            String foundedNumber;
+            String replacedNumber;
+            Pattern pattern = Pattern.compile("(\\d{4}\\s\\d{4}\\s\\d{4}\\s\\d{4})");
+            while ((line = bufferedReader.readLine()) != null) {
+                Matcher matcher = pattern.matcher(line);
+                while (matcher.find()) {
+                    foundedNumber = matcher.group();
+                    replacedNumber = foundedNumber.split(" ")[0] + " **** **** " + foundedNumber.split(" ")[3];
+                    line = line.replaceAll(foundedNumber, replacedNumber);
                 }
-                bufferedReader.close();
-                fileInputStream.close();
-            } catch (IOException | URISyntaxException e) {
-                e.printStackTrace();
+                result.append(line);
             }
-            return result.toString();
-        } else {
-            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return result.toString();
     }
 
     /**
@@ -57,33 +48,26 @@ public class SimpleRegExpService implements RegExpService {
     @Override
     public String replacePlaceholders(double paymentAmount, double balance) {
         StringBuilder result = new StringBuilder();
-        URL resource = this.getClass().getResource("/sensitive_data.txt");
-        if (resource != null) {
-            try {
-                FileInputStream fileInputStream = new FileInputStream(Paths.get(resource.toURI()).toFile());
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
-                String line;
-                String foundedAmount;
-                String foundedBalance;
-                Pattern pattern = Pattern.compile(".*(?<amount>\\$\\{payment_amount\\}).*(?<balance>\\$\\{balance\\}).*");
-                while ((line = bufferedReader.readLine()) != null) {
-                    Matcher matcher = pattern.matcher(line);
-                    while (matcher.find()) {
-                        foundedAmount = matcher.group("amount");
-                        foundedBalance = matcher.group("balance");
-                        line = line.replace(foundedAmount, String.format("%.0f", paymentAmount));
-                        line = line.replace(foundedBalance, String.format("%.0f", balance));
-                    }
-                    result.append(line);
+        File file = new File("src/main/resources/sensitive_data.txt".replaceAll("/", File.separator));
+        try (FileInputStream fileInputStream = new FileInputStream(file);
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream, StandardCharsets.UTF_8))) {
+            String line;
+            String foundedAmount;
+            String foundedBalance;
+            Pattern pattern = Pattern.compile(".*(?<amount>\\$\\{payment_amount\\}).*(?<balance>\\$\\{balance\\}).*");
+            while ((line = bufferedReader.readLine()) != null) {
+                Matcher matcher = pattern.matcher(line);
+                while (matcher.find()) {
+                    foundedAmount = matcher.group("amount");
+                    foundedBalance = matcher.group("balance");
+                    line = line.replace(foundedAmount, String.format("%.0f", paymentAmount));
+                    line = line.replace(foundedBalance, String.format("%.0f", balance));
                 }
-                bufferedReader.close();
-                fileInputStream.close();
-            } catch (IOException | URISyntaxException e) {
-                e.printStackTrace();
+                result.append(line);
             }
-            return result.toString();
-        } else {
-            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return result.toString();
     }
 }
